30d25078b62c12037571a96fc618a385f0d98bc8
Dragos Resetnic <developer@macmini.local>
2014-07-01 17:47:17 +0300

    added mail core to prototype

--- VVM_MOC/VMViewController.m
+++ VVM_MOC/VMViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "VMViewController.h"
+#import <MailCore/MailCore.h>
 
 @interface VMViewController ()
 
@@ -17,6 +18,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    MCOIMAPSession *session = [[MCOIMAPSession alloc] init];
+    [session setHostname:@"imap.google.com"];
+    [session setPort:993];
+    [session setUsername:@"developer.admatica@gmail.com"];
+    [session setPassword:@"developeratadmatica"];
+    [session setConnectionType:MCOConnectionTypeStartTLS];
+    
+    MCOIMAPMessagesRequestKind requestKind = MCOIMAPMessagesRequestKindHeaders;
+    NSString *folder = @"INBOX";
+    MCOIndexSet *uids = [MCOIndexSet indexSetWithRange:MCORangeMake(1, UINT64_MAX)];
+    
+    MCOIMAPFetchMessagesOperation *fetchOperation = [session fetchMessagesByUIDOperationWithFolder:folder requestKind:requestKind uids:uids];
+    
+    [fetchOperation start:^(NSError * error, NSArray * fetchedMessages, MCOIndexSet * vanishedMessages) {
+        //We've finished downloading the messages!
+        
+        //Let's check if there was an error:
+        if(error) {
+            NSLog(@"Error downloading message headers:%@", error);
+        }
+        
+        //And, let's print out the messages...
+        NSLog(@"The post man delivereth:%@", fetchedMessages);
+    }];
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
